6738d2d9b0e01cf0b2ac117311c75cfbb0e56aa0
adnane <31042158+adnanebk@users.noreply.github.com>
2022-01-22 07:51:18 -0800

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  path :    'https://ang-shop.herokuapp.com/api/',
-  //path: 'http://localhost:8080/api/',
+  //path :    'https://ang-shop.herokuapp.com/api/',
+  path: 'http://localhost:8080/api/',
 
   pathDoc: 'https://ang-shop.herokuapp.com/swagger-ui/',
   //pathDoc : 'http://localhost:8080/swagger-ui/',
